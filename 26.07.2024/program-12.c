b93c3140de3505479a0edb3b96b4d02eba453079
deekshanacs <145849222+deekshanacs@users.noreply.github.com>
2024-07-26 20:05:58 +0530

    program-12.c

---
+++ 26.07.2024/program-12.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+int main()
+{
+  int m1,m2,m3,avg;
+  scanf("%d",&m1);
+  scanf("%d",&m2)
+  scanf("%d",&m3)
+  avg=(m1+m2+m3)/3
+  if (avg>=90)
+  {
+    printf("The grade is A\n);
+ }
+  else if(avg>80 & avg<=90)     
+ {
+      printf("The grade is B\n");
+}
+else if(avg>70 & avg<=80)
+{
+  printf("The garde is C\n");
+}
+else if(avg>60 &avg<=70){
+  printf("the grade is D\n")
+}
+  else if(avg>50 & avg<=60){
+    printf("the grade is E\n");
+  }
+  else
+  {
+    printf("the grade is F\n");
+  }
+}
