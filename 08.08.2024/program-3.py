3d45f086d2b6f82328ae533453efdc0f3b835d2f
deekshanacs <145849222+deekshanacs@users.noreply.github.com>
2024-08-08 23:42:13 +0530

    program-3.py

---
+++ 08.08.2024/program-3.py
@@ -0,0 +1,11 @@
+def word(str):
+    counts = dict()
+    words = str.split()
+    for word in words:
+        if word in counts:
+            counts[word] += 1
+        else:
+            counts[word] = 1
+
+    return counts
+print( word_count(input("enter a string : "))
