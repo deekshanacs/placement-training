9c4fb18840680c5a8ee27a580d8018ee41121479
deekshanacs <145849222+deekshanacs@users.noreply.github.com>
2024-08-08 23:36:40 +0530

    program-2.py

---
+++ 08.08.2024/program-2.py
@@ -0,0 +1,7 @@
+def odd(str):
+    result = ""
+    for i in range(len(str)):
+        if i % 2 == 0:
+            result = result + str[i]
+    return result
+print(odd('input("Enter a string : "))  
